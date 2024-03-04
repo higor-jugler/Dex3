769a0f07d2b98b4badfe9bd00cf18fbeb84b038b
Higor <higorjugler@hotmail.com>
2024-03-04 10:10:51 -0300

    Inicio da criacao da tela de detalhes

---
+++ Dex3/PokemonDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonDetail.swift
+//  Dex3
+//
+//  Created by Higor Jugler on 3/4/24.
+//
+
+import SwiftUI
+
+struct PokemonDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonDetail()
+}
---
+++ Dex3/SamplePokemon.swift
@@ -0,0 +1,8 @@
+//
+//  SamplePokemon.swift
+//  Dex3
+//
+//  Created by Higor Jugler on 3/4/24.
+//
+
+import Foundation
