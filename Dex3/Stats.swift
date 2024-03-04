c7b6ac673c9936de9c45a37958d790c503a88faa
Higor <higorjugler@hotmail.com>
2024-03-04 14:26:04 -0300

    Implementado tela de status

---
+++ Dex3/Stats.swift
@@ -0,0 +1,18 @@
+//
+//  Stats.swift
+//  Dex3
+//
+//  Created by Higor Jugler on 3/4/24.
+//
+
+import SwiftUI
+
+struct Stats: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Stats()
+}
