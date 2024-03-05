ade867e49c1becac7912107959e37340a176b8d2
Higor <higorjugler@hotmail.com>
2024-03-05 17:04:18 -0300

    App funcionando porem faltou a implementacao do widget

---
+++ Dex3/FetchedImage.swift
@@ -0,0 +1,18 @@
+//
+//  FetchedImage.swift
+//  Dex3
+//
+//  Created by Higor Jugler on 3/5/24.
+//
+
+import SwiftUI
+
+struct FetchedImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FetchedImage()
+}
---
+++ Dex3/WidgetPokemon.swift
@@ -0,0 +1,18 @@
+//
+//  WidgetPokemon.swift
+//  Dex3
+//
+//  Created by Higor Jugler on 3/5/24.
+//
+
+import SwiftUI
+
+struct WidgetPokemon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WidgetPokemon()
+}
